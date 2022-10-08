677ae2d7b794f83feaad379d3e6c8c90edcb11e7
Alejandra Rojas <alejandra.rojas@hyperisland.se>
2022-10-08 12:58:29 +0200

    minimize logo on scroll

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// Reducing header height on scroll
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 50) {
+    document.getElementById("logo").style.width = "80px";
+    document.getElementById("logo").style.height = "80px";
+  } else {
+    document.getElementById("logo").style.width = "140px";
+    document.getElementById("logo").style.height = "140px";
+  }
+}
+
+// Hamburger Menu Mobile
+let toggleButton = document.getElementById('ham-button');
+let navList = document.getElementById('ham-active');
+
+toggleButton.addEventListener('click', () => {
+  navList.classList.toggle('ham-activeb');
+})
+
+
+// Active menu on scroll
+
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav .container ul li");
 window.onscroll = () => {
