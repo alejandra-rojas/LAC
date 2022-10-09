2cc38da034e8743e4ab3adf72a702a0e1ad73dd8
Alejandra Rojas <alejandra.rojas@hyperisland.se>
2022-10-09 11:42:33 +0200

    style stages

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// Reducing header height on scroll by reducing size of logo
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 50) {
-    document.getElementById("logo").style.width = "70px";
-    document.getElementById("logo").style.height = "70px";
-  } else {
-    document.getElementById("logo").style.width = "140px";
-    document.getElementById("logo").style.height = "140px";
-  }
-}
 
 // Hamburger Menu Mobile
 let toggleButton = document.getElementById('ham-button');
