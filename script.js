5974af6b47939fafcb807642635d18a506c0c113
Alejandra Rojas <alejandra.rojas@hyperisland.se>
2022-10-12 19:32:16 +0200

    Update-replacingindexandstyles

--- script.js
+++ script.js
@@ -28,22 +28,3 @@ window.onscroll = () => {
     }
   });
 };
-
-
-// TRIGGERING CSS ANIMATION ON SCROLL
-
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    const square = entry.target.querySelector('.backimg-cont');
-
-    if (entry.isIntersecting) {
-      square.classList.add('bounce-animation');
-	  return; // if we added the class, exit the function
-    }
-
-    // We're not intersecting, so remove the class!
-    square.classList.remove('bounce-animation');
-  });
-});
-observer.observe(document.querySelector('.animation-wrapper'));
